8ab6d0ca08723805b746e01f865489a76ee654dc
cesar mendez <devcsar@gmail.com>
2020-06-28 19:53:57 +0300

    added new tool

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace netschool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var versionString = Assembly.GetEntryAssembly()
+                                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                        .InformationalVersion
+                                        .ToString();
+
+                Console.WriteLine($"NetSchool v{versionString}");
+                Console.WriteLine("-------------");
+                // Console.WriteLine("\nUsage:");
+                // Console.WriteLine("  botsay <message>");
+                ShowBot(string.Join(' ', args));
+                return;
+            }
+
+            // ShowBot(string.Join(' ', args));
+        }
+        static void ShowBot(string message)
+        {
+            string bot = $"\n        {message}";
+            bot += @"
+    __________________
+                      \
+                       \
+ _   _      _            _                 _ 
+| \ | |    | |          | |               | |
+|  \| | ___| |_ ___  ___| |__   ___   ___ | |
+| . ` |/ _ \ __/ __|/ __| '_ \ / _ \ / _ \| |
+| |\  |  __/ |_\__ \ (__| | | | (_) | (_) | |
+\_| \_/\___|\__|___/\___|_| |_|\___/ \___/|_|
+";
+            Console.WriteLine(bot);
+        }
+    }
+}
